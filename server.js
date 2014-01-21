0c440b634809a5e0b50eef3eb5395624d37e38e9
Andrey Petrov <andrey.petrov@shazow.net>
2014-01-20 17:18:31 -0800

    Working example.

--- server.js
+++ server.js
@@ -6,22 +6,40 @@ var PORT = 8000;
 
 
 function renderElement(page, selector) {
-    page.clipRect = page.evaluate(function(selector) {
+    var clipRect = page.evaluate(function(selector) {
         return document.querySelector(selector).getBoundingClientRect();
     }, selector);
 
+    page.clipRect = {
+        top:    clipRect.top,
+        left:   clipRect.left,
+        width:  clipRect.width,
+        height: clipRect.height
+    };
+
     var pic = page.renderBase64('png');
+
+    console.log("Rendered image of size: " + clipRect.width + 'x' + clipRect.height + " (" + pic.length + " bytes)");
+
     return pic;
 };
 
 
 console.log("Starting server on port " + PORT + ".");
 webserver.create().listen(PORT, function(request, response) {
+    console.log(request.method + ' ' + request.url);
+
+    if (request.method != 'POST' || request.url != '/') {
+        response.statusCode = 404;
+        response.close();
+        return;
+    }
+
     // TODO: Add some kind of security here?
     var render_fn = request.post && request.post['src'];
     var page = webpage.create();
 
-    page.open('d3shell.html', function(status) {
+    page.open('shell.html', function(status) {
         if (render_fn) {
             this.evaluate(new Function(render_fn));
         }
@@ -35,10 +53,10 @@ webserver.create().listen(PORT, function(request, response) {
                 'Content-Type': 'image/png'
             };
             response.setEncoding('binary');
-            response.write(atob(this.renderBase64('png')));
+            response.write(atob(r));
         } else {
             response.statusCode = 500;
-            response.write("Something went wrong.");
+            response.write("No image rendered after evaluating: \n" + render_fn);
         }
 
         response.close();
